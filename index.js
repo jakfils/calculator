d1174579dd826d00c583d945870ae43f738a2c97
Your Name <jakfilstechno@gmail.com>
2022-06-03 19:56:26 +0100

    Fix problems in the code

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 /* Variables*/
 const buttonOperations = document.querySelectorAll(".buttonOperation");
 const buttonClear = document.querySelector(".buttonClear");
-let expression = "";F
+let expression = "";
+let res = 0;
+const resultElt = document.getElementById("resultElt");
+
+/* button clear*/
+buttonClear.addEventListener("click", (e) => {
+  resultElt.textContent = "0";
+  expression = 0;
+});
+
+/*Operations*/
+buttonOperations.forEach((buttonOperation) => {
+  buttonOperation.addEventListener("click", (e) => {
+    if (e.target.value === "=") {
+      res = eval(expression);
+      res = String(res);
+      resultElt.textContent = res.replace(".", ",");
+      if (expression !== 0) {
+        expression = res;
+      } else {
+        expression = 0;
+      }
     } else if (e.target.value !== "=") {
       expression += e.target.value;
       resultElt.textContent = expression.replace(".", ",");
-      console.log(expression.replace("0",""));
+      console.log(expression.replace("0", ""));
     }
   });
 });
